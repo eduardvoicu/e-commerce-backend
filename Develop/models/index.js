bbc4f81359f0d050f6e5e6996bd73bc0eb63f8d3
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-15 22:44:35 -0600

    changed something around in index.js/models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -8,15 +8,19 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
@@ -29,3 +33,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
